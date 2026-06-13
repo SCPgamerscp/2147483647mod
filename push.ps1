204fbc4e6fb1bd099e4e2203269993ed8a2d8e88
creature-creature <168889478+creature-creature@users.noreply.github.com>
2026-06-13 11:29:23 +0900

    chore: remove helper ps1 script from repo

--- push.ps1
+++
@@ -1,8 +0,0 @@
-cd "C:\Users\ecrea\Downloads\2147483647mod"
-git init
-git add -A
-git commit -m "Initial commit: 2147483647mod (item stack / enchant level / potion amplifier up to Integer.MAX_VALUE)"
-git branch -M main
-git remote add origin https://github.com/SCPgamerscp/2147483647mod.git
-git push -u origin main
-Write-Host "DONE"
